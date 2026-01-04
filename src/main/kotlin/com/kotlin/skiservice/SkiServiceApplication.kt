b5aad5d3140004553467d3ab126f78a4c8c24084
Бачище Назар Сергеевич <mcarim@mail.ru>
2026-01-04 20:05:19 +0300

    initial commit

---
+++ src/main/kotlin/com/kotlin/skiservice/SkiServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.skiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkiServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SkiServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/skiservice/SkiServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.skiservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkiServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
